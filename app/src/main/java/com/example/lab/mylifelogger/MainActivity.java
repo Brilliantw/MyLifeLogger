5b6a3a8f702a85035a1eff62d3a13313db71e0ab
lab <mjw95@kookmin.ac.kr>
2016-11-28 20:06:13 +0900

    Toggle Button Enabled & Make MapActivity

--- app/src/main/java/com/example/lab/mylifelogger/MainActivity.java
+++ app/src/main/java/com/example/lab/mylifelogger/MainActivity.java
@@ -1,9 +1,23 @@
 package com.example.lab.mylifelogger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 import android.widget.ToggleButton;
+import android.content.Context;
+import android.view.View;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import static com.example.lab.mylifelogger.R.styleable.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
     ToggleButton bt_fall;
     Button bt_log;
     Button bt_check;
+    Intent intent_map;
+    Intent intent_fall;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +42,17 @@ public class MainActivity extends AppCompatActivity {
         bt_log = (Button) findViewById(R.id.Logbt);
         bt_check = (Button) findViewById(R.id.Checkbt);
 
+        intent_map = new Intent(this, MapsActivity.class);
+
+        bt_gps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ( bt_gps.isChecked() ) {
+                    startActivity(intent_map);
+                }
+            }
+        });
     }
+
+
 }
